5f92a8b61d1b367ba8c8137cb6dd5a56a68d38f5
rem1rem1 <rem1rem1@mail.ru>
2019-10-10 17:24:17 +0200

    Кнопка "Архивировать"

--- local/js/wcomm.js
+++ local/js/wcomm.js
@@ -1,19 +1,14 @@
 wcomm_PopupWindowCA = null;
+wcomm_PopupWindowCA2 = null;
 wcomm_CALL_OWNER_TYPE_ID = null;
 wcomm_CALL_OWNER_ID = null;
 
-//console.log("=1111=");
+WCOMM_CALL_BIZ_PROC_ID = 20;
+
 
 BX.addCustomEvent('onAfterActivitySave', function(params)
 {
-	
-	//console.log("=2222=");
-		
-	//BX.CrmTimelineMenuBar.openMenu();
-	//BX.message('CRM_TIMELINE_SMS_REST_MARKETPLACE')
-	//BX.SidePanel.Instance.open
-		
-		
+
 	var request = BX.ajax.runAction('wcomm:callmodifications.api.activityajax.callsexist', {
 		data: {
 			param1: '1'
@@ -22,9 +17,9 @@ BX.addCustomEvent('onAfterActivitySave', function(params)
 		 
 	request.then(function(response){
 		
-		console.log("=3333=");	
+		//console.log("=3333=");	
 			
-			if(('status' in response)&&(response['status'] == 'success')&&('data' in response)&&('data' in response)&&('show' in response['data']))
+			if(('status' in response)&&(response['status'] == 'success')&&('data' in response)&&('show' in response['data']))
 			{
 				
 				if(response['data']['show'] == 'true')
@@ -96,23 +91,25 @@ function CallAddPopupWindowPrepareContent()
 		cell.appendChild(successButton);
 		var successId = "successID";
 		BX.CrmSubscriber.subscribe(
-				'2322_' + successId,
-				successButton, "click", function()
-					{
-						//"CALL";
-						planner = new BX.Crm.Activity.Planner();
-						planner.showEdit(
-							{
-								"TYPE_ID": BX.CrmActivityType.call,
-								"OWNER_TYPE_ID": wcomm_CALL_OWNER_TYPE_ID, //BX.CrmEntityType.enumeration.deal,
-								"OWNER_ID": wcomm_CALL_OWNER_ID
-							}
-						);
+			'2322_' + successId,
+			successButton,
+			"click",
+			function()
+			{
+					//"CALL";
+					planner = new BX.Crm.Activity.Planner();
+					planner.showEdit(
+						{
+							"TYPE_ID": BX.CrmActivityType.call,
+							"OWNER_TYPE_ID": wcomm_CALL_OWNER_TYPE_ID, //BX.CrmEntityType.enumeration.deal,
+							"OWNER_ID": wcomm_CALL_OWNER_ID
+						}
+					);
+							
+					wcomm_PopupWindowCA.close();
 						
-						wcomm_PopupWindowCA.close();
-						
-					} ,
-				BX.CrmParamBag.create({ id: successId, preventDefault: true })
+			} ,
+			BX.CrmParamBag.create({ id: successId, preventDefault: true })
 		);
 	}
 			
@@ -138,8 +135,10 @@ function CallAddPopupWindowPrepareContent()
 		var failureId = "failureID";
 		BX.CrmSubscriber.subscribe(
 			'2322_' + failureId,
-			failureButton, "click", function()
-				{
+			failureButton, 
+			"click", 
+			function()
+			{
 					//"MEETING";
 					planner = new BX.Crm.Activity.Planner();
 					planner.showEdit(
@@ -152,7 +151,72 @@ function CallAddPopupWindowPrepareContent()
 					
 					wcomm_PopupWindowCA.close();
 						
-				},
+			},
+			BX.CrmParamBag.create({ id: failureId, preventDefault: true })
+		);
+	}
+	
+	
+
+	if(wcomm_CALL_OWNER_TYPE_ID == BX.CrmEntityType.enumeration.company)
+	{
+
+		failureTitle = BX.message('CRM_WCOMM_TO_ARCHIVE');
+		var failureButton = BX.create(
+				"A",
+				{
+					//"webform-small-button webform-small-button-decline"
+					attrs: { className: "webform-small-button webform-small-button-accept", href: "#" },
+					children:
+					[
+						BX.create("SPAN", { attrs: { className: "webform-small-button-left" } }),
+						BX.create("SPAN", { attrs: { className: "webform-small-button-text" }, text: failureTitle }),
+						BX.create("SPAN", { attrs: { className: "webform-small-button-right" } })
+					]
+				}
+		);
+		cell.appendChild(failureButton);
+		var failureId = "failureID";
+		BX.CrmSubscriber.subscribe(
+			'2322_' + failureId,
+			failureButton, 
+			"click", 
+			function()
+			{
+				
+				wcomm_PopupWindowCA.close();
+				
+				var request = BX.ajax.runAction('wcomm:callmodifications.api.activityajax.archivecampaign', {
+					data: {
+						CompanyID: wcomm_CALL_OWNER_ID
+					}
+				});
+					 
+				request.then(function(response){
+					
+					if(('status' in response)&&(response['status'] == 'success')&&('data' in response)&&('ok' in response['data']))
+					{
+						
+						wcomm_PopupWindowCA2 = new BX.PopupWindow(
+							"CallAddPopupWindow",
+							null,
+							{
+								"zIndex": 90001,
+								"closeByEsc": true,
+								"autoHide": false,
+								"offsetLeft": -50,
+								"closeIcon": false,
+								"overlay": {backgroundColor: 'black', opacity: '80' },
+								"className": "crm-list-end-deal",
+								"content": CallAddPopupWindowPrepareContent2(response['data']['ok'] == 'true')							
+							}
+						);
+						wcomm_PopupWindowCA2.show();
+					}
+					
+				});
+				
+			},
 			BX.CrmParamBag.create({ id: failureId, preventDefault: true })
 		);
 	}
@@ -160,3 +224,70 @@ function CallAddPopupWindowPrepareContent()
 			
 	return WrapperPC;
 }
+
+
+
+function CallAddPopupWindowPrepareContent2(success)
+{
+	WrapperPC = BX.create("DIV");
+	
+	var table = BX.create("TABLE",
+		{
+			attrs: { className: "crm-list-end-deal-block" },
+			props: { cellSpacing: "0", cellPadding: "0", border: "0" }
+		}
+	);
+	WrapperPC.appendChild(table);
+
+	var cell = table.insertRow(-1).insertCell(-1);
+	cell.className = "crm-list-end-deal-text";
+	
+	if(success)
+	{
+		cell.innerHTML = BX.message('CRM_WCOMM_TO_ARCHIVE_OK');
+	} else {
+		cell.innerHTML = BX.message('CRM_WCOMM_TO_ARCHIVE_ERR');
+	}
+
+	cell = table.insertRow(-1).insertCell(-1);
+	cell.className = "crm-list-end-deal-buttons-block";
+
+	
+	if(true)
+	{
+		var successText = BX.message('CRM_WCOMM_CLOSE_BUTTON');
+		var successButton = BX.create(
+			"A",
+			{
+				attrs: { className: "webform-small-button webform-small-button-accept", href: "#" },
+				children:
+				[
+						BX.create("SPAN", { attrs: { className: "webform-small-button-left" } }),
+						BX.create("SPAN", { attrs: { className: "webform-small-button-text" }, text: successText }),
+						BX.create("SPAN", { attrs: { className: "webform-small-button-right" } })
+				]
+			}
+		);
+					
+		cell.appendChild(successButton);
+		var successId = "successID";
+		BX.CrmSubscriber.subscribe(
+			'2323_' + successId,
+			successButton,
+			"click",
+			function()
+			{							
+					wcomm_PopupWindowCA2.close();
+						
+			} ,
+			BX.CrmParamBag.create({ id: successId, preventDefault: true })
+		);
+	}
+			
+			
+			
+	return WrapperPC;
+}
+
+
+
--- local/modules/wcomm.callmodifications/lib/controller/activityajax.php
+++ local/modules/wcomm.callmodifications/lib/controller/activityajax.php
@@ -59,6 +59,42 @@ class ActivityAjax extends Controller
 		return ['show' => 'false'];
 	}
 	
+	/**
+	 * @param string $param1
+	 * @return array
+	 */
+	public static function archivecampaignAction($CompanyID = '0')
+	{
+		$CompanyID = intval($CompanyID);
+				
+		if(($CompanyID > 0) && \Bitrix\Main\Loader::IncludeModule("crm"))
+		{
+						
+			$strUserFieldCode = 'UF_CRM_1568628386291';
+			$strArchivalXMLID = '301400e7c947c60680079f02ffa87570';
+			
+			$arrUF = \CCrmCompany::GetUserFields($CompanyID);
+			if(isset($arrUF[$strUserFieldCode]))
+			{				
+				$UFID = $arrUF[$strUserFieldCode]['ID'];
+				$arrUFVar = \CUserFieldEnum::GetList(array(), array("USER_FIELD_ID" => $UFID, 'XML_ID' => $strArchivalXMLID));
+				
+				if($arrArchival = $arrUFVar->GetNext())
+				{					
+					$arFields = array(
+						$strUserFieldCode => $arrArchival['ID']
+					);
+					
+					$GLOBALS['USER_FIELD_MANAGER']->Update('CRM_COMPANY', $CompanyID, $arFields);
+					return ['ok' => 'true'];
+				}
+			}
+		}
+		
+		return ['ok' => 'false'];
+		
+	}
+	
 	static function GetHLiBlockID($HLiBlockName)
 	{
 		
