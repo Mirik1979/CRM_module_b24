fa19590ca597bdb598ae0f265b27a0f899f2f1ef
Miros <lya-job@yandex.ru>
2019-11-09 09:53:03 +0300

    корректировки меню

--- local/components/wcomm.crmstores/stores.menu/lang/ru/.description.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["CRM_STORES_MENU_NAME"] = "Панель инструментов";
-$MESS["CRM_STORES_MENU_DESCRIPTION"] = "Панель инструментов";
-$MESS["CRM_NAME"] = "CRM";
-$MESS["CRM_COMPANY_NAME"] = "Компании";
-?>
